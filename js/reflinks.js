ed2cf254601e7cd50e105612543269db24f824d1
Thomas Van Parys <thpar@psb.ugent.be>
2017-09-04 17:49:02 +0200

    Further styling of refs and abstract toggle

--- js/reflinks.js
+++ js/reflinks.js
@@ -1,7 +1,13 @@
 (function($){
-    
-
-
+    $(function(){
+	    var $ugent_bib_ref = $(".ugent-bib-ref");
+	   	    
+	    function showAbstract(){
+		$abstract = $(this).find(".ugent-bib-abstract");
+		$abstract.toggle(200);
+	    }
+	    
+	    $ugent_bib_ref.on("click", showAbstract);
+	});
 
 }(jQuery));
-
