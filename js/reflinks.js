9beb23f9ba4f68cb3b687a86b47efca9523d57ae
Thomas Van Parys <thpar@psb.ugent.be>
2017-09-05 14:11:58 +0200

    Make 'Abstract' also into a link

--- js/reflinks.js
+++ js/reflinks.js
@@ -1,13 +1,24 @@
 (function($){
     $(function(){
 	    var $ugent_bib_ref = $(".ugent-bib-ref");
+	    var $ugent_bib_abstract_link = $(".ugent-bib-abstract-link");
+
 	   	    
 	    function showAbstract(){
 		$abstract = $(this).find(".ugent-bib-abstract");
 		$abstract.toggle(200);
 	    }
+
+	    function abstractLinkClicked(){
+		$ugent_bib = $(this).parent().parent();
+		$abstract = $ugent_bib.find(".ugent-bib-abstract");
+		$abstract.toggle(200);
+		return false;
+	    }
 	    
 	    $ugent_bib_ref.on("click", showAbstract);
+	    $ugent_bib_abstract_link.on("click", abstractLinkClicked);
+
 	});
 
 }(jQuery));
